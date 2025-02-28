6350a4976cf41fae2a507c856f12c53d9384d592
Mauro Mendoza <msmendozael@icloud.com>
2025-02-27 22:15:32 -0300

    feat(api): add exceptions and improve /predict responses

--- challenge/model.py
+++ challenge/model.py
@@ -4,7 +4,6 @@ from typing import List, Tuple, Union
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 
--- tests/stress/api_stress.py
+++ tests/stress/api_stress.py
@@ -13,5 +13,6 @@ class StressUser(HttpUser):
     @task
     def predict_latam(self):
         self.client.post(
-            "/predict", json={"flights": [{"OPERA": "Grupo LATAM", "TIPOVUELO": "N", "MES": 3}]}
+            "/predict",
+            json={"flights": [{"OPERA": "Grupo LATAM", "TIPOVUELO": "N", "MES": 3}]},
         )
